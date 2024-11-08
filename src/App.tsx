67638bb88fde81ca6bbda3859b81747a546b7163
Deora Awantika <deoraawantika@helpdesknonotobukkukonpyuta-6.local>
2024-11-08 16:35:24 +0900

    removed unnecessary syntax and renamed files

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import Practice1 from "./components/Practice1";
 function App() {
 	return (
 		<div>
-		<div className="box-border pb-80 ml-60 mr-60 mt-20 h-200 w-104 border-4 ..."> 
+		<div className="box-border pb-80 mx-60 mt-20 h-200 w-104 border-4"> 
 		<div className="bg-slate-400 text-center p-[20px] text-4xl text-gray-50">
 			React-v3
 		</div>
-		<div className='mt-14 mb-50 flex space-x-20 flex-row items-center justify-center'>
 		<Practice1 />
-		</div>
 		</div>
 		</div>
 	);
