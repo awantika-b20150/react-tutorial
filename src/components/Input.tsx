425a67ece3298aceecfd781eda1a486f7cb32736
Deora Awantika <deoraawantika@helpdesknonotobukkukonpyuta-6.local>
2024-11-12 09:31:21 +0900

    changed to arrow functions for callbacks

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,14 +5,17 @@ import Button from './Button';
 const SearchInput=()=> {
     const [value, setValue] = useState<string>("");
 
-    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>)=> {
         setValue(event.currentTarget.value);
       }
-    function handleClick()
-    {
-      !!value && console.log(value);
-      setValue("");
-    }
+      const handleClick = () => 
+      {
+          if (value) 
+            {
+              console.log(value);
+              setValue("");
+            }
+      }
   return (
     <>
       <input type='search' onClick={handleClick} value={value} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-60 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500" />
