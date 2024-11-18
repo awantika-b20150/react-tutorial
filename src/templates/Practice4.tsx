9e0ae5bdd9246cd27e8a58cbccaeac69e2354480
Deora Awantika <deoraawantika@helpdesknonotobukkukonpyuta-6.local>
2024-11-19 08:40:06 +0900

    removed the double display of required error message, now it only comes under search button

--- src/templates/Practice4.tsx
+++ src/templates/Practice4.tsx
@@ -80,7 +80,7 @@ const Practice4: React.FC = () => {
             id="postalCode" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-60 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500"
             {...register('postalCode')}
           />
-          {errors.postalCode && (
+          {canSearch && errors.postalCode && (
             <p className="text-xs italic text-red-500">{errors.postalCode.message}</p>
           )}
           </div>
