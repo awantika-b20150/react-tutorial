a28aa8d064ad7651826a669c6ec9c7b5180e1e82
Deora Awantika <deoraawantika@helpdesknonotobukkukonpyuta-6.local>
2024-11-20 09:12:48 +0900

    separated the table header component

--- src/hooks/useToDoList.ts
+++ src/hooks/useToDoList.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 
-interface Data {
+export interface Data {
   id: number;
   todayDate: string;
   taskName: string;
--- src/templates/Practice5.tsx
+++ src/templates/Practice5.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useToDoList } from '../hooks/useToDoList';
 import UserInput from '../components/Input';
+import TableHeader from '../components/TableHeader';
 
 const Practice5: React.FC = () => {
   const {todos,isAllSelected,task,setTask,addTask,deleteTask,selectAll,toggleCheckbox,deleteSelected} = useToDoList();
@@ -11,29 +12,8 @@ const Practice5: React.FC = () => {
       <div className="mb-4">
         {todos.length > 0 ? (
           <table className="w-full table-auto border-collapse mt-8 text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead className="text-xl border-b-2 border-gray-300 py-2 px-4 bg-gray-100">
-              <tr>
-                <th className="relative text-xl font-bold text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <input
-                    type="checkbox"
-                    checked={isAllSelected}
-                    onChange={selectAll}
-                  />
-                  {todos.some(todo => todo.isChecked) && (
-                    <button
-                      onClick={deleteSelected}
-                      className="absolute top-[-50px] px-2 py-1 w-16 h-8 text-xs bg-red-500 text-white rounded-md"
-                    >
-                      一括削除
-                    </button>
-                  )}
-                </th>
-                <th>登録日</th>
-                <th>内容</th>
-                <th>操作</th>
-              </tr>
-            </thead>
-            <tbody>
+          <TableHeader checked={isAllSelected} todos={todos} onChange={selectAll} onClick={deleteSelected}/>
+          <tbody>
               {todos.map(todo => (
                 <tr key={todo.id} className="border-b border-r py-2 px-4">
                   <td className="border-b border-r py-2 px-4">
